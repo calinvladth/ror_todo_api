546e02b2cc60495827de488fdb3c923becc0b9a7
calinvladth <calinvladth@icloud.com>
2022-06-14 09:33:19 +0300

    another error

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
   namespace :api, defaults: { format: :json } do
     namespace :v1 do
+      devise_for :users, controllers: { sessions: :sessions },
+                 path_names: { sign_in: :login }
+      resource :user, only: [:show, :update]
       resources :todos
     end
   end
-
 end
